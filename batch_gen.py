5f2fec2e818f6f01c17add166b0fa44f488a322a
Abdul Majeed Alkattan <abedelmajeedk@gmail.com>
2023-06-08 20:03:19 +0200

    next batch

--- batch_gen.py
+++ batch_gen.py
@@ -36,53 +36,54 @@ class BatchGenerator(object):
         file_ptr.close()
         random.shuffle(self.list_of_examples) # Shuffle the list of examples randomly after reading them.
 
-# This function returns the input, output and mask tensors for the next batch of data based on the batch size.
-def next_batch(self, batch_size):
-    # Take the subset of examples from self.list_of_examples based on batch_size.
-    batch = self.list_of_examples[self.index:self.index + batch_size]
-    self.index += batch_size # Update the pointer after taking the subset.
-    
-    # Initialize the input and target lists for each example in the batch.
-    batch_input = []
-    batch_target = []
-    
-    # Loop through each example in the batch.
-    for vid in batch:
-        # Load the features and ground-truth file for the current example using numpy.
-        features = np.load(self.features_path + vid.split('.')[0] + '.npy')
-        file_ptr = open(self.gt_path + vid, 'r')
-        content = file_ptr.read().split('\n')[:-1]
-        classes = np.zeros(min(np.shape(features)[1], len(content)))
+    # This function returns the input, output and mask tensors for the next batch of data based on the batch size.
+    def next_batch(self, batch_size):
+        print("batch_size", batch_size)
+        # Take the subset of examples from self.list_of_examples based on batch_size.
+        batch = self.list_of_examples[self.index:self.index + batch_size]
+        self.index += batch_size # Update the pointer after taking the subset.
         
-        # Map the action words to integer labels using actions_dict for all the sentences in the ground-truth file.
-        for i in range(len(classes)):
-            classes[i] = self.actions_dict[content[i]]
+        # Initialize the input and target lists for each example in the batch.
+        batch_input = []
+        batch_target = []
+        
+        # Loop through each example in the batch.
+        for vid in batch:
+            # Load the features and ground-truth file for the current example using numpy.
+            features = np.load(self.features_path + vid.split('.')[0] + '.npy')
+            file_ptr = open(self.gt_path + vid, 'r')
+            content = file_ptr.read().split('\n')[:-1]
+            classes = np.zeros(min(np.shape(features)[1], len(content)))
             
-        # Append the features and classes lists with self.sample_rate steps as inputs and outputs respectively.
-        batch_input.append(features[:, ::self.sample_rate])
-        batch_target.append(classes[::self.sample_rate])
+            # Map the action words to integer labels using actions_dict for all the sentences in the ground-truth file.
+            for i in range(len(classes)):
+                classes[i] = self.actions_dict[content[i]]
+                
+            # Append the features and classes lists with self.sample_rate steps as inputs and outputs respectively.
+            batch_input.append(features[:, ::self.sample_rate])
+            batch_target.append(classes[::self.sample_rate])
 
-    # Compute length of sequences for all the targets and store in a variable.
-    length_of_sequences = map(len, batch_target)
-    
-    # Initialize the input tensor with zeros using torch for all the examples in the batch.
-    batch_input_tensor = torch.zeros(len(batch_input), np.shape(batch_input[0])[0], max(length_of_sequences), dtype=torch.float)
-    
-    # Initialize the target tensor with -100 using torch for all the examples in the batch.
-    batch_target_tensor = torch.ones(len(batch_input), max(length_of_sequences, default=0), dtype=torch.long)*(-100)
-    
-    # Initialize the mask tensor with zeros using torch for all the examples in the batch.
-    mask = torch.zeros(len(batch_input), self.num_classes, max(length_of_sequences, default=0), dtype=torch.float)
-    
-    # Loop through each example in the batch. 
-    for i in range(len(batch_input)):
-        # Fill the input tensor with the features and current length of sequence.
-        batch_input_tensor[i, :, :np.shape(batch_input[i])[1]] = torch.from_numpy(batch_input[i])
+        # Compute length of sequences for all the targets and store in a variable.
+        length_of_sequences = map(len, batch_target)
         
-        # Fill the target tensor with the classes and current length of sequence.
-        batch_target_tensor[i, :np.shape(batch_target[i])[0]] = torch.from_numpy(batch_target[i])
+        # Initialize the input tensor with zeros using torch for all the examples in the batch.
+        batch_input_tensor = torch.zeros(len(batch_input), np.shape(batch_input[0])[0], max(length_of_sequences), dtype=torch.float)
         
-        # Set the mask tensor to ones where we have ground-truth samples along the sequence.
-        mask[i, :, :np.shape(batch_target[i])[0]] = torch.ones(self.num_classes, np.shape(batch_target[i])[0])
+        # Initialize the target tensor with -100 using torch for all the examples in the batch.
+        batch_target_tensor = torch.ones(len(batch_input), max(length_of_sequences, default=0), dtype=torch.long)*(-100)
+        
+        # Initialize the mask tensor with zeros using torch for all the examples in the batch.
+        mask = torch.zeros(len(batch_input), self.num_classes, max(length_of_sequences, default=0), dtype=torch.float)
+        
+        # Loop through each example in the batch. 
+        for i in range(len(batch_input)):
+            # Fill the input tensor with the features and current length of sequence.
+            batch_input_tensor[i, :, :np.shape(batch_input[i])[1]] = torch.from_numpy(batch_input[i])
+            
+            # Fill the target tensor with the classes and current length of sequence.
+            batch_target_tensor[i, :np.shape(batch_target[i])[0]] = torch.from_numpy(batch_target[i])
+            
+            # Set the mask tensor to ones where we have ground-truth samples along the sequence.
+            mask[i, :, :np.shape(batch_target[i])[0]] = torch.ones(self.num_classes, np.shape(batch_target[i])[0])
 
-    return batch_input_tensor, batch_target_tensor, mask
+        return batch_input_tensor, batch_target_tensor, mask
